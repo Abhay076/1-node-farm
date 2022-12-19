1d89bb06ce7cfca4e51c8712d24d45a139580c37
Abhay 076 <yadavabhaycs@gmail.com>
2022-12-19 19:33:54 +0530

    creating a simple web server

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
+
 const fs = require("fs");
-//for reading in file
-//using blocking system to read and write file
-const inputText = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(inputText);
+const http = require('http');
+// //for reading in file
+// //using blocking system to read and write file
+// const inputText = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(inputText);
 
-//for writing in file
-//using blocking system to read and write file
-const textOut = `this is what we know about ${inputText} and ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File Written!!");
+// //for writing in file
+// //using blocking system to read and write file
+// const textOut = `this is what we know about ${inputText} and ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File Written!!");
 
-// using NON-Blocking system to read and write file Aync
+// // using NON-Blocking system to read and write file Aync
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.log("error");
-    return;
-  }
-  console.log(data);
-  fs.writeFile("./txt/finals.txt",`${data}`,'utf-8', (err) => {
-    console.log("Your file has been written!!");
-  });
-});
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log("error");
+//     return;
+//   }
+//   console.log(data);
+//   fs.writeFile("./txt/finals.txt",`${data}`,'utf-8', (err) => {
+//     console.log("Your file has been written!!");
+//   });
+// });
 
-fs.writeFile('./txt/finalss.txt','./txt/output.txt',err=>{
-    console.log('File wrtitten!!');
+// fs.writeFile('./txt/finalss.txt','./txt/output.txt',err=>{
+//     console.log('File wrtitten!!');
+// })
+// console.log("ReadFile!!!");
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+const server = http.createServer((req,res)=>{
+  res.end('Hello world!')
 })
-console.log("ReadFile!!!");
+server.listen(8000,(err)=>{
+  console.log("Server has been created on port: 8000");
+})
