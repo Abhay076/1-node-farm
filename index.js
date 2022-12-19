2a996c9019a49a40f0d7f65d4e5f1637f2208851
Abhay 076 <yadavabhaycs@gmail.com>
2022-12-19 19:13:14 +0530

    reading and writing files Asynchronously

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const fs = require('fs');
+const fs = require("fs");
 //for reading in file
-const inputText  = fs.readFileSync('./txt/input.txt','utf-8');
+//using blocking system to read and write file
+const inputText = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(inputText);
 
 //for writing in file
-const textOut =`this is what we know about ${inputText} and ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt',textOut);
-console.log('File Written!!');
+//using blocking system to read and write file
+const textOut = `this is what we know about ${inputText} and ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File Written!!");
+
+// using NON-Blocking system to read and write file Aync
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("error");
+    return;
+  }
+  console.log(data);
+  fs.writeFile("./txt/finals.txt",`${data}`,'utf-8', (err) => {
+    console.log("Your file has been written!!");
+  });
+});
+
+fs.writeFile('./txt/finalss.txt','./txt/output.txt',err=>{
+    console.log('File wrtitten!!');
+})
+console.log("ReadFile!!!");
